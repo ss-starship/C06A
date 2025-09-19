3f85e5927731915c321648b664bceb204ed74d6d
Sarah <sabunnell@mines.edu>
2025-09-18 18:46:34 -0600

    general saving (taking a break)

--- Burner.java
+++ Burner.java
@@ -7,39 +7,58 @@ public class Burner {
 		WARM,
 		COLD
 	}
-	
+
 	private Temperature myTemperature;
 	Setting mySetting; //don't know its privacy
-	int timer;
+	int timer = 0;
 	//end variables
-	
+
 	//constructor
 	public Burner() {
 		myTemperature = Temperature.COLD;
 		mySetting = Setting.OFF;
 	}
-	
+
 	//getter
 	public Temperature getMyTemperature() {
 		return myTemperature;
 	}
-	
+
 	//functions
 	public void plusButton() {
-		// TODO Auto-generated method stub
-		
+		switch (mySetting) {
+		case OFF:
+			mySetting = Setting.LOW;
+		case LOW:
+			mySetting = Setting.MEDIUM;
+		case MEDIUM:
+			mySetting = Setting.HIGH;
+			//case HIGH, setting HIGH
+		default:
+			break;
+		}
+		timer = TIME_DURATION;
 	}
 
 	public void minusButton() {
-		// TODO Auto-generated method stub
-		
+		switch (mySetting) {
+		//case OFF, setting OFF
+		case LOW:
+			mySetting = Setting.OFF;
+		case MEDIUM:
+			mySetting = Setting.LOW;
+		case HIGH:
+			mySetting = Setting.MEDIUM;
+		default:
+			break;
+		}
+		timer = TIME_DURATION;
 	}
 
 	public void updateTemperature() {
-		// TODO Auto-generated method stub
-		
+		//timer -= 1;
 	}
-	
+
 	public void display() {
 		System.out.println(mySetting + "....." + myTemperature);
 	}
